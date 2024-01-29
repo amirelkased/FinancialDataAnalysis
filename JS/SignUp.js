9005ba701ca64890a16489a6cfc9cdc2c4926eb1
maryam zaghlool <maryamzaghlool0@gmail.com>
2024-01-29 23:44:44 +0200

    all done

--- JS/SignUp.js
+++ JS/SignUp.js
@@ -1,10 +1,28 @@
-document.getElementById("button").onclick = function () {
-    let pass = document.getElementsByClassName("password")[0];
-    if (pass.length !== 10) {
-        document.getElementById("error").style.display = "block";
-        console.log("ok");
+let form = document.getElementsByClassName("form")[0];
+let inputs = document.getElementsByClassName("inputs");
+let error = document.getElementsByClassName("error");
+let button = document.getElementById("button");
+let count = 0;
+form.onsubmit = function x(e) {
+    let pass = document.getElementsByClassName("password")[0].value;
+    for (let x = 0; x < inputs.length; x++) {
+        if ((inputs.length-1).value !== '' && inputs[x].value !== ' ' && pass.length >= 10 && pass.match(/[0-9]/i) && /[_$]/g.test(pass) && pass.match(/[A-Z]/) && pass.match(/[a-z]/) ) {
+            error[x].style.display = "none";
+        }
+        else {
+            for (let y = 0; y < inputs.length; y++) {
+                if (inputs[y].value !== '' && inputs[y].value !== ' ')
+                    error[y].style.display = "none";
+            }
+            error[x].style.display = "block";
+            e.preventDefault();
+            window.history.back();
+        }
+        if (inputs[x].value !== '' && inputs[x].value !== ' ') {
+            count++;
+        }
     }
-    else {
-        document.getElementById("error").style.display = "none";
-    }
+}
+if (count === inputs.length) {
+    document.getElementsByClassName("form")[0].submit();
 }
